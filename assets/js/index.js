cede403edd058251aef109411fba187340868e3f
deepakSahu <121280456+dsahu8634deepak@users.noreply.github.com>
2023-10-15 18:09:03 +0530

    SETAP PROJECT

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,26 @@
-// include Header
+// -----------------------------------------include Header--------------------------------
 $(function () {
     $("#header").load("layout/header.html");
     $("#footer").load("layout/footer.html");
 });
-// include Header
-// header color change
+//****************************************** */ include Header****************************
+
+
+//---------------------------------------- use it header color change---------------------
 $(function () {
     $(window).on("scroll", function () {
         if ($(window).scrollTop() > 70) {
             $(".NavbarMain").addClass("active");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
+      //remove the background property so it comes transparent again (defined in your css)
             $(".NavbarMain").removeClass("active");
         }
     });
 });
-// header color change
+//*****************************************/ header color change****************************
 
-// owl
+
+// ----------------------------------------owl carousel javascript--------------------------
 $('.slider').owlCarousel({
     loop: true,
     autoplay: true,
@@ -40,38 +43,8 @@ $('.slider').owlCarousel({
         }
     }
 })
+//*******************************************owl Carousel End*******************************
 
-// 
-const readMoreButton = document.getElementById("readMoreButton");
-const hiddenParagraphs = document.querySelector(".hidden-paragraphs");
-
-let isOpen = false; // Variable to track the state
-
-readMoreButton.addEventListener("click", function () {
-  if (!isOpen) {
-    hiddenParagraphs.style.maxHeight = "1000px"; // Set a large max-height value to show the paragraphs
-    readMoreButton.textContent = "Read Less";
-  } else {
-    hiddenParagraphs.style.maxHeight = "0"; // Set max-height back to 0 to hide the paragraphs
-    readMoreButton.textContent = "Read More";
-  }
-  isOpen = !isOpen; // Toggle the state
-});
-const readMoreButton2 = document.getElementById("readMoreButton2");
-const hiddenParagraphs2 = document.querySelector(".hidden-paragraphs2");
-
-let isOpen2 = false; // Variable to track the state
-
-readMoreButton2.addEventListener("click", function () {
-  if (!isOpen2) {
-    hiddenParagraphs2.style.maxHeight = "1000px"; // Set a large max-height value to show the paragraphs
-    readMoreButton2.textContent = "Read Less";
-  } else {
-    hiddenParagraphs2.style.maxHeight = "0"; // Set max-height back to 0 to hide the paragraphs
-    readMoreButton2.textContent = "Read More";
-  }
-  isOpen2 = !isOpen2; // Toggle the state
-});
 
 
 
